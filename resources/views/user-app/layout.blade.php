3897e79777d0accbead76851388d5da9700c1910
devitcoderz <dev.itcoderz@gmail.com>
2025-06-04 17:31:37 +0500

    a4-6-2025 asdfa

--- resources/views/user-app/layout.blade.php
+++ resources/views/user-app/layout.blade.php
@@ -42,7 +42,9 @@
     <link rel="manifest" href="{{ asset('manifest.json') }}">
     <script>
         if ('serviceWorker' in navigator) {
-            navigator.serviceWorker.register("{{ asset('public/sw.js') }}");
+            navigator.serviceWorker.register('/sw.js')
+                .then(() => console.log('Service Worker registered'))
+                .catch(err => console.error('Service Worker error:', err));
         }
     </script>
 </head>
