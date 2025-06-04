a3dc585bbd2142294dcc9e93a2227d5ceda058f8
devitcoderz <dev.itcoderz@gmail.com>
2025-06-04 18:07:39 +0500

    a4-6-2025 iuiutb

--- resources/views/user-app/layout.blade.php
+++ resources/views/user-app/layout.blade.php
@@ -41,8 +41,14 @@
     {{--@laravelPWA--}}
     <link rel="manifest" href="{{ asset('manifest.json') }}">
     <script>
+        {{--if ('serviceWorker' in navigator) {--}}
+        {{--    navigator.serviceWorker.register("{{ asset('public/sw.js') }}");--}}
+        {{--}--}}
+
         if ('serviceWorker' in navigator) {
-            navigator.serviceWorker.register("{{ asset('public/sw.js') }}");
+            navigator.serviceWorker.register('/sw.js', { scope: '/' })
+                .then(() => console.log('✅ Service Worker registered globally'))
+                .catch(err => console.error('❌ Service Worker error:', err));
         }
     </script>
 </head>
