315e3aa4dbc2b494ee5fd69a8df59808ecc617e5
devitcoderz <dev.itcoderz@gmail.com>
2025-06-04 17:10:52 +0500

    a4-6-2025 map error solved

--- resources/views/user-app/track-ride.blade.php
+++ resources/views/user-app/track-ride.blade.php
@@ -31,47 +31,46 @@
     <!-- bottom navbar end -->
 
     <!-- sidebar starts -->
-    @include('user-app.partials.sidear')
+    @include('user-app.partials.sidebar')
     <!-- sidebar end -->
 
     @endsection
 
-<script>
-    let map, marker;
-
-    function initMap() {
-        map = new google.maps.Map(document.getElementById("map"), {
-            zoom: 14,
-            center: { lat: 30.1575, lng: 71.5249 }
-        });
-
-        marker = new google.maps.Marker({
-            map: map,
-            icon: 'https://maps.google.com/mapfiles/ms/icons/blue-dot.png'
-        });
-
-        pollDriverLocation();
-    }
-
-    function pollDriverLocation() {
-        setInterval(() => {
-            var url = '{{ url('user/get-driver-location') }}'
-            fetch(url)
-                .then(res => res.json())
-                .then(data => {
-                    const latLng = new google.maps.LatLng(data.lat, data.lng);
-                    marker.setPosition(latLng);
-                    map.panTo(latLng);
-                });
-        }, 5000); // Poll every 5 seconds
-    }
-
-    window.initMap = initMap;
-</script>
-
-<script src="https://maps.googleapis.com/maps/api/js?key=AIzaSyBKqq-XxVccy3MdBiolKZOJ601LNqvFPaE&callback=initMap" async defer></script>
-
-
     @section('script')
 
-@endsection
+        <script>
+            let map, marker;
+
+            function initMap() {
+                map = new google.maps.Map(document.getElementById("map"), {
+                    zoom: 14,
+                    center: { lat: 30.1575, lng: 71.5249 }
+                });
+
+                marker = new google.maps.Marker({
+                    map: map,
+                    icon: 'https://maps.google.com/mapfiles/ms/icons/blue-dot.png'
+                });
+
+                pollDriverLocation();
+            }
+
+            function pollDriverLocation() {
+                setInterval(() => {
+                    var url = '{{ url('user/get-driver-location') }}'
+                    fetch(url)
+                        .then(res => res.json())
+                        .then(data => {
+                            const latLng = new google.maps.LatLng(data.lat, data.lng);
+                            marker.setPosition(latLng);
+                            map.panTo(latLng);
+                        });
+                }, 5000); // Poll every 5 seconds
+            }
+
+            window.initMap = initMap;
+        </script>
+
+        <script src="https://maps.googleapis.com/maps/api/js?key=AIzaSyBKqq-XxVccy3MdBiolKZOJ601LNqvFPaE&callback=initMap" async defer></script>
+
+    @endsection
